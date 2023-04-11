3feb57cec94644b7bdf778c62c8b528b96368d12
CristinaOsorio <macry_08@hotmail.com>
2023-04-10 22:24:32 -0600

    Added token verification functionality. Added version of node to package.json

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,5 +1,18 @@
 const VerifyToken = (req, res ) => {
-  res.send("VerifyToken")
+
+  try {
+    let accessToken = "p8xuH!n0=JeDx1j7hSUVv=O2pgbjHEAJgfciuyPW4qb5N6mbnhKUI/M!/V91NDBgz0Zs8n5TRqbj8v=Ji?1d2jTAq=f86qwDpNoL0YFsMKB!Xlytasv2eKsLSo00=SDGtIJDsZzAJ/nuW1pSOI150aSl7BAq3AX90hpK3!QSqn-P-wMQ5ACFZ1PPkVq8ZqMk!-qjR89rKgyI3bLvuyj?1x3XzP8tloMDHMbTtDekWDpHZ6t-/pQFO4q=aIg5x9EN";
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (challenge != null && token != null && accessToken) {
+      res.send(challenge)
+    } else {
+      res.status(400).send() 
+    }
+  } catch (error) {
+    res.status(400).send()
+  }
 }
 
 
